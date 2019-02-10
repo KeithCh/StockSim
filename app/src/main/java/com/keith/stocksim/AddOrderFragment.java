065becfc4828c1ccb12ca841637e10592bce915c
Shadowlord69 <keithchow101@yahoo.com>
2019-02-10 17:16:38 -0500

    added navbar

---
+++ app/src/main/java/com/keith/stocksim/AddOrderFragment.java
@@ -0,0 +1,17 @@
+package com.keith.stocksim;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddOrderFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_add_order, container, false);
+    }
+}
---
+++ app/src/main/java/com/keith/stocksim/Navbar.java
@@ -0,0 +1,12 @@
+package com.keith.stocksim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Navbar extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
